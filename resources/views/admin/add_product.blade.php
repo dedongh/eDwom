60839f0361993634831378f6e4b6045db4c5d956
KAY <slimshades68@gmail.com>
2020-03-09 18:25:19 +0000

    Product CRUD Operations - Upload Product Image (Admin)

--- resources/views/admin/add_product.blade.php
+++ resources/views/admin/add_product.blade.php
@@ -49,7 +49,7 @@
                         <div class="control-group">
                             <label class="control-label" for="selectError3">Category</label>
                             <div class="controls">
-                                <select id="selectError3" name="category_id">
+                                <select id="selectError3" name="category_id" required>
                                     <option >Select category</option>
                                     <?php
                                     $all_published_category = DB::table('tbl_category')
@@ -66,7 +66,7 @@
                         <div class="control-group">
                             <label class="control-label" for="selectError3">Brand</label>
                             <div class="controls">
-                                <select id="selectError3" name="brand_id">
+                                <select id="selectError3" name="brand_id" required>
                                     <option >Select brand</option>
                                     <?php
                                     $all_published_brands = DB::table('tbl_brands')
@@ -92,7 +92,7 @@
                         <div class="control-group hidden-phone">
                             <label class="control-label" for="category_description">Product short description</label>
                             <div class="controls">
-                                <textarea class="cleditor" name="product_short_description" rows="3"></textarea>
+                                <textarea class="cleditor" name="product_short_description" rows="3" required></textarea>
                             </div>
                         </div>
 
